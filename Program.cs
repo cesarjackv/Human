8ba745b9df97e804fd2de5ab0db1a63b2114a931
Cesar Jack-Vallee <cesarjackv@gmail.com>
2022-09-26 02:10:05 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Human{
+    // Properties for Human
+    public string Name;
+    public int Strength;
+    public int Intelligence;
+    public int Dexterity;
+    public int Health;
+    // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+    public Human(string name){
+        Name = name;
+        Strength = 3;
+        Intelligence = 3;
+        Dexterity = 3;
+        Health = 100;
+    }
+    // Add a constructor to assign custom values to all fields
+    public Human(string name, int str, int intel, int dex, int hp){
+        Name = name;
+        Strength = str;
+        Intelligence = intel;
+        Dexterity = dex;
+        Health = hp;
+    }
+    
+    // Build Attack method
+    public int Attack(Human target){
+        int dmg = Strength * 3;
+        target.Health -= dmg;
+        Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+        return target.Health;
+    }
+}
+
